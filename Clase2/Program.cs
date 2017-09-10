54fdef064717256fe47748c1ba0571a19640d4f9
DanielCarbajal314 <daniel.carbajal@pucp.edu.pe>
2017-09-10 09:54:47 -0500

    Initial Commit

---
+++ Clase2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+using Clase2.DAL;
+
+namespace Clase2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServicioDeProductos servicioProducto = new ServicioDeProductos();
+            servicioProducto.ListarTodosLosProductos().ForEach(producto => Console.WriteLine(producto.Nombre));
+            Console.ReadKey();
+        }
+    }
+}
